d9dc248b7d910d43d2bd9889e42778f4f6846b2b
avaunt <12223621+AVaunt-consulting@users.noreply.github.com>
2025-04-18 12:15:41 +0100

    Fix SvelteKit Netlify adapter configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,11 @@
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: { 
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather than using standard Node-based functions
-			edge: false,
-			split: false // Make sure all routes are bundled in the same function
-		}),
+		adapter: adapter(),
 		files: {
 			assets: 'static'
 		}
